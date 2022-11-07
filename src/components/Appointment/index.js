a0e27f8bd8e8ba902155e640b5e1d7cd1b0f2ca9
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-07 09:51:04 -0800

    add constants for error handling , with second parameter true to be able to replace mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Empty from "./Empty";
 import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
+import Error from "./Error";
 import Confirm from "./Confirm";
 
 const EMPTY = "EMPTY";
@@ -16,6 +17,8 @@ const SAVING = "SAVING";
 const DELETING = "DELETE";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 function Appointment(props) {
   let student = undefined;
@@ -37,17 +40,23 @@ function Appointment(props) {
     };
     transition(SAVING);
 
-    props.bookInterview(props.id, interview).then(() => {
-      transition(SHOW);
-    });
+    props
+      .bookInterview(props.id, interview)
+      .then(() => {
+        transition(SHOW);
+      })
+      .catch(() => transition(ERROR_SAVE, true));
   }
 
   function remove() {
-    transition(DELETING);
+    transition(DELETING, true);
 
-    props.cancelInterview(props.id).then(() => {
-      transition(EMPTY);
-    });
+    props
+      .cancelInterview(props.i)
+      .then(() => {
+        transition(EMPTY);
+      })
+      .catch(() => transition(ERROR_DELETE, true));
   }
 
   return (
@@ -89,6 +98,13 @@ function Appointment(props) {
           onSave={save}
           onCancel={back}
         />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error message="Could not save appointment." onClose={back} />
+      )}
+
+      {mode === ERROR_DELETE && (
+        <Error message="Could not cancel appointment." onClose={back} />
       )}
     </article>
   );
