705be6605215dfe3fb194e5b2b991039c1879add
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-09 14:00:33 -0800

    add beforeach for things that happens to all tests. Visit home etc

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-undef */
 describe("Appointments", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
+  });
+  it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones")
